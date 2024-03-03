de505bb3d9dc93a2d6980b6c005b06f50d14026d
santtuniskanen <santtuniska02@gmail.com>
2024-03-03 17:38:52 +0200

    added packages

--- app/src/main/java/com/example/weather/network/WeatherApiService.kt
+++ app/src/main/java/com/example/weather/network/WeatherApiService.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object WeatherApiService {
-    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
